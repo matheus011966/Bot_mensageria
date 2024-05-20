846a02b713cc55da7444a0a1990c3ca4ee5d89f6
matheus011966 <matheus011966@gmail.com>
2024-05-20 17:47:25 -0300

    fix: correção no envio da mensagem após carregar a img

--- Bot_Mensageria.py
+++ Bot_Mensageria.py
@@ -79,14 +79,16 @@ for i, mensagem in enumerate(contato_df['Mensagem']):
         time.sleep(5)
         attach = navegador.find_element("xpath", '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div/div/span/div/ul/div/div[2]/li')#.send_keys(midia)
         attach.click()
-        time.sleep(5)
+        time.sleep(10)
         keyboard = Controller()
         keyboard.type(file_path)
         keyboard.press(Key.enter)
-        keyboard.release(Key.enter)
         time.sleep(5)
-        send = navegador.find_element("xpath", '//*[@id="app"]/div/div[2]/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]/div/div')
-        send.click()
+        keyboard.press(Key.enter)
+        time.sleep(20)
+        #send = 
+        #navegador.find_element("xpath", '//*[@id="app"]/div/div[2]/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]/div/div').click()
+        #send.click()
         #aguarda
         time.sleep(5)
         Mensagens_enviadas = Mensagens_enviadas + 1
